af94644be830f8f3dbd2359a25e7838d089aac0b
ardiG12 <abduraxmonmuxamedov7@gmail.com>
2025-09-08 21:18:25 +0500

    Model User

--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    class Rank(models.TextChoices):
+        E = "E", "E"
+        D = "D", "D"
+        C = "C", "C"
+        B = "B", "B"
+        A = "A", "A"
+        S = "S", "S"
+
+    rank = models.CharField(
+        max_length=1,
+        choices=Rank.choices,
+        default=Rank.E,
+    )
+    level = models.IntegerField(default=1)
+    experience = models.IntegerField(default=0)
+
+    # статы
+    strength = models.IntegerField(default=0)
+    intelligence = models.IntegerField(default=0)
+    dexterity = models.IntegerField(default=0)
+    endurance = models.IntegerField(default=0)
+    luck = models.IntegerField(default=0)
